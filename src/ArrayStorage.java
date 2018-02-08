9897be7b556c33f83323c2596c4bd827ade27599
ABaranchikov <ryoken@mail.ru>
2018-02-08 23:47:25 +0300

    update

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,30 +1,70 @@
 /**
  * Array based storage for Resumes
+ *@count - счетчик объектов в массиве
  */
+import java.util.Arrays;
+
 public class ArrayStorage {
-    Resume[] storage = new Resume[10000];
+    private Resume[] storage = new Resume[10000];
+    private int count = 0;
 
-    void clear() {
+    public void clear() {
+
+        Arrays.fill(this.storage, null);
+        count = 0;
+
     }
 
-    void save(Resume r) {
+    public void update(Resume r){
+//проверить на наличие резюме
+
     }
 
-    Resume get(String uuid) {
+    public void save(Resume r) {
+        //проверить на отсутствие резюме
+        if (count< storage.length) {
+            storage[count++] = r;
+        }
+        else System.out.println("Storage is full! Can't add new element.");
+    }
+
+    public Resume get(String uuid) {
+
+        for (int i=0; i<count; i++){
+            if (storage[i].getUuid().equals(uuid)) {
+                return storage[i];
+            }
+        }
+
         return null;
     }
 
-    void delete(String uuid) {
+    public void delete(String uuid) {
+        //проверить на наличие резюме
+
+        for (int i=0; i<count; i++){
+            if (storage[i].getUuid().equals(uuid) ) {
+              //  System.arraycopy(storage,i+1, storage, i, count-i-1);
+                storage[i] = storage[count-1];
+                storage[--count] =null;
+                break;
+            }
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
-    Resume[] getAll() {
-        return new Resume[0];
+    public Resume[] getAll() {
+        return Arrays.copyOf(storage, count);
     }
 
-    int size() {
-        return 0;
+    public int size() {
+
+        return count;
     }
 }
+//убрать дублированеи кода
+
+   // Сделать проверки: в update/delete/get - резюме есть в storage, в save- нет в storage: System.out.println("Resume ...").
+     //   Сделать в save проверку на переполнениеe: System.out.println("...").
