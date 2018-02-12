5efae670bbba398249ca2d1957834e3eead87ca9
Baranchikov Alexander <ryoken@mail.ru>
2018-02-12 22:04:27 +0300

    Add files via upload

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -2,56 +2,116 @@
  * Array based storage for Resumes
  *@count - счетчик объектов в массиве
  */
+import com.sun.org.apache.xpath.internal.SourceTree;
+
 import java.util.Arrays;
 
 public class ArrayStorage {
-    Resume[] storage = new Resume[10000];
+    private Resume[] storage = new Resume[10000];
     private int count = 0;
 
-    void clear() {
+    /*
+    возвращает индекс элемента массива, если он найден.
+    В противном случае возвращает -1.
+    @param String uuid - ID элемента массива
+     */
+    public int getIndex(String uuid) {
+
+        for (int i = 0; i < count; i++) {
+            if (storage[i].getUuid().equals(uuid)) return i;
+        }
+        return -1;
+    }
+
+    public int getIndex(Resume r) {
+
+        for (int i = 0; i < count; i++) {
+            if (storage[i].equals(r)) return i;
+        }
+        return -1;
+    }
+
+
+
+    /*
+    Очищает массив
+     */
+    public void clear() {
 
         Arrays.fill(this.storage, null);
         count = 0;
 
     }
 
-    void save(Resume r) {
+    /*
+    проверяет массив на наличие резюме.
+    При наличии - его обновляет.
+    Пока реализован поиск. Что здесь можно обновить - не понятно.
+     */
+
+    public void update(Resume r){
+
+        String id = r.getUuid();
+
+        if (getIndex(r)>=0) {
+            System.out.println("Нашли резюме");
+        }
+        else System.out.println("Данного резюме нет в базе!");
+    }
+
+    /*
+    Сохраняем резюме в базе
+    @param Resume r - резюме
+     */
+    public void save(Resume r) {
+        //проверить на отсутствие резюме
+        int i = getIndex(r);
+        if (i>=0) {
+            System.out.println("Данное резюме уже есть в базе!");
+            return;
+        }
+
         if (count< storage.length) {
             storage[count++] = r;
         }
         else System.out.println("Storage is full! Can't add new element.");
     }
+    /*
+    получить резюме по ID
+     */
+    public Resume get(String uuid) {
 
-    Resume get(String uuid) {
+        int i = getIndex(uuid);
 
-        for (int i=0; i<count; i++){
-            if (storage[i].uuid == uuid) {
-                return storage[i];
-            }
-        }
+        if (i>=0) {return storage[i];}
+        else System.out.println("Данного резюме нет в базе!");
 
         return null;
     }
+    /*
+    удалить резюме по ID
+     */
+    public void delete(String uuid) {
+        //проверить на наличие резюме
 
-    void delete(String uuid) {
-
-        for (int i=0; i<count; i++){
-            if (storage[i].uuid == uuid) {
-                System.arraycopy(storage,i+1, storage, i, count-i-1);
-                storage[--count] =null;
-                break;
-            }
+        int i =getIndex(uuid);
+        if (i>=0) {
+            storage[i] = storage[count-1];
+            storage[--count] =null;
         }
+        else System.out.println("Данного резюме нет в базе!");
+
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
-    Resume[] getAll() {
+    public Resume[] getAll() {
         return Arrays.copyOf(storage, count);
     }
 
-    int size() {
+    public int size() {
+
         return count;
     }
 }
--- src/Resume.java
+++ src/Resume.java
@@ -4,10 +4,24 @@
 public class Resume {
 
     // Unique identifier
-    String uuid;
+    private String uuid;
 
     @Override
     public String toString() {
         return uuid;
+    }
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+
+    @Override
+    //нужно будетв переопределить в следующем уроке
+    public boolean equals(Object o) {
+        return super.equals(o);
     }
 }
