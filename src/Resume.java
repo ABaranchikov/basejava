a5709bfbbbfe28c9233442e060c1982413f068a2
ABaranchikov <ryoken@mail.ru>
2018-02-13 22:20:49 +0300

    update

--- src/Resume.java
+++ src/Resume.java
@@ -20,7 +20,7 @@ public class Resume {
     }
 
     @Override
-    //нужно будетв переопределить в следующем уроке
+    //нужно будет переопределить в следующем уроке
     public boolean equals(Object o) {
         return super.equals(o);
     }
