09996de46916a5dd926c36b6f3331772970eff37
ABaranchikov <ryoken@mail.ru>
2018-02-27 00:07:25 +0300

    update sortedArrayStorage

--- src/SortedArrayStorage.java
+++ src/SortedArrayStorage.java
@@ -1,50 +1,42 @@
 import java.util.Arrays;
 
-public class SortedArrayStorage extends AbstractArrayStorage{
+public class SortedArrayStorage extends AbstractArrayStorage {
 
+    /*
+    Add a resume in sorted order.
+     */
     @Override
     public void save(Resume r) {
         int i = getIndex(r.getUuid());
         if (i >= 0)
             System.out.println("Resume is allready exist!");
-        else if (size+1 > STORAGE_LIMIT)
+        else if (size + 1 > STORAGE_LIMIT)
             System.out.println("Storage is full! Can't add new element.");
         else {
-            int newIndex = -i-1;
-            System.arraycopy(storage, newIndex, storage, newIndex+1, size-newIndex);
+            int newIndex = -i - 1;
+            System.arraycopy(storage, newIndex, storage, newIndex + 1, size - newIndex);
             storage[newIndex] = r;
             size++;
         }
     }
 
-    @Override
-    public void update(Resume r) {
-        int i = getIndex(r.getUuid());
-        if (i<0) {
-            System.out.println("Resume not found!");
-        }
-        else {
-            storage[i] = r;
-        }
-    }
-
+    /*
+    Delete resume from base and save the sorted order
+     */
     @Override
     public void delete(String uuid) {
         int i = getIndex(uuid);
-        if (i<0) {
+        if (i < 0) {
             System.out.println("Resume not found!");
-        }
-        else {
-            System.arraycopy(storage, i+1, storage, i, size-i-1);
+        } else {
+            System.arraycopy(storage, i + 1, storage, i, size - i - 1);
             storage[size--] = null;
         }
     }
 
-    @Override
-    public Resume[] getAll() {
-        return 	Arrays.copyOfRange(storage,0,size);
-    }
-
+    /*
+    Search index of element using binarySearch
+    */
     @Override
     protected int getIndex(String uuid) {
         Resume searchKey = new Resume();
