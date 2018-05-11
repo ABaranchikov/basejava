717234dfb5cb952dea7f1ade467da6d484c3c43a
ABaranchikov <ryoken@mail.ru>
2018-05-11 07:26:43 +0300

    lesson 11: new variant2

--- src/ru/javawebinar/basejava/TestDeadlock.java
+++ src/ru/javawebinar/basejava/TestDeadlock.java
@@ -1,46 +1,42 @@
 package ru.javawebinar.basejava;
 
+import static java.lang.Thread.sleep;
+
 public class TestDeadlock {
     private static final Object LOCK1 = new Object();
-    private static volatile boolean isCheck = false;
+    private static final Object LOCK2 = new Object();
 
     private static class Task1 implements Runnable {
         @Override
         public void run() {
-            System.out.println("Task1 run");
             synchronized (LOCK1) {
-                System.out.println("Task1 locked LOCK1");
-                isCheck = true;
-                    try {
-                        System.out.println("Task1 wait (isCheck = " + isCheck + ")");
-                        while (isCheck) {
-                            LOCK1.wait();
-                        }
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                LOCK1.notify();
-                System.out.println("Task1 notify (isCheck= " + isCheck + ")");
+                System.out.println(Thread.currentThread().getName() + " lock LOCK1");
+                try {
+                    sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println(Thread.currentThread().getName() + " try get LOCK2");
+                synchronized (LOCK2) {
+                    System.out.println(Thread.currentThread().getName() + " lock LOCK2");
+                }
             }
         }
     }
 
     private static class Task2 implements Runnable {
         public void run() {
-            System.out.println("Task2 run");
-            synchronized (LOCK1) {
-                System.out.println("Task2 locked LOCK1");
-                isCheck = false;
+            synchronized (LOCK2) {
+                System.out.println(Thread.currentThread().getName() + " lock LOCK2");
                 try {
-                    System.out.println("Task2 wait (isCheck = " + isCheck + ")");
-                    while (!isCheck) {
-                        LOCK1.wait();
-                    }
+                    sleep(500);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
-                LOCK1.notify();
-                System.out.println("Task2 notify (isCheck= " + isCheck + ")");
+                System.out.println(Thread.currentThread().getName() + " try get LOCK1");
+                synchronized (LOCK1) {
+                    System.out.println(Thread.currentThread().getName() + " lock LOCK1");
+                }
             }
         }
     }
